9752d56a8ce3ba11f5c8a7dfc84a8e4b3e651d19
realizelol <bsw.bsw@gmx.de>
2025-10-17 16:16:48 +0200

    [Test] No logs? 2

--- rootfs/etc/cont-init.d/03-create-service.sh
+++ rootfs/etc/cont-init.d/03-create-service.sh
@@ -5,16 +5,15 @@
 mkdir -p /etc/services.d/pure-ftpd
 
 cat > /etc/services.d/pure-ftpd/run <<EOF
-#!/usr/bin/execlineb -P
-with-contenv
-pure-ftpd${PUREFTPD_FLAGS:-$(cat /var/run/s6/container_environment/PUREFTPD_FLAGS)}
+#!/usr/bin/with-contenv sh
+pure-ftpd${PUREFTPD_FLAGS:-$(cat /var/run/s6/container_environment/PUREFTPD_FLAGS)} 2>&1
 EOF
 chmod +x /etc/services.d/pure-ftpd/run
 
 mkdir -p /var/run/s6/services
 cat > /etc/services.d/pure-ftpd/finish <<EOF
 #!/usr/bin/with-contenv sh
-echo >&2 "pure-ftpd exited. code=${1}"
+echo >&2 "pure-ftpd exited. code=${?}"
 exec s6-svscanctl -t /var/run/s6/services
 EOF
 chmod +x /etc/services.d/pure-ftpd/finish
